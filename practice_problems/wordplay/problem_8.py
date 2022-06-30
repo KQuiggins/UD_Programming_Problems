2b0024fc59b6f9d6679ca9427ed619775549110a
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2022-06-30 08:12:29 -0400

    renamed files and completed problem 8

---
+++ practice_problems/wordplay/problem_8.py
@@ -0,0 +1,10 @@
+""" What are all of the words with no vowel and not even a Y? """
+
+with open('sowpods.txt', 'r') as f:
+    words = f.read().splitlines()
+    for word in words:
+        if 'A' in word or 'E' in word or 'I' in word or 'O' in word or 'U' in word or 'Y' in word:
+            continue
+        else:
+            print(word)
+            break
