c972c9ba91504df39ea42a9aea0d2ccd7748165a
KQuiggins <kashfamilyvalues@gmail.com>
2022-08-11 20:38:46 -0400

    completed problem 6 countries

---
+++ practice_problems/BabyNames/prob_3_bNames.py
@@ -0,0 +1,6 @@
+""" There is at least one baby name from the top 40 baby names for 2020 that, when spelled backwards, is a valid Scrabble word. Find and print all such names. """
+
+with open('bNames2020.txt', 'r') as f:
+    names = f.read().splitlines()
+    
+    
--- practice_problems/countries/prob_6_countries.py
+++ practice_problems/countries/prob_6_countries.py
@@ -1,5 +1,28 @@
 """ There is at least one country name that contains another country name. Find all of these cases. """
 
+found = []
 
 
+with open('countries.txt', 'r') as f:
+    countries = f.read().splitlines()
+    for i in range(len(countries)):
+        for j in range(i + 1, len(countries)):
+            if countries[i] in countries[j]:
+                found.append((countries[i], countries[j]))
+
+print(found)
+print(len(found))
+
+            
+        
+        
+
+
+
+
+        
+
+            
+
+        
     
