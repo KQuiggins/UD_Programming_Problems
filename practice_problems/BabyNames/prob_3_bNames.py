aa6eb162f7f31d9472d39d9b2c4621b0d396ba58
KQuiggins <kashfamilyvalues@gmail.com>
2022-08-22 19:08:50 -0400

    completed problem 3 in Baby Names

--- practice_problems/BabyNames/prob_3_bNames.py
+++ practice_problems/BabyNames/prob_3_bNames.py
@@ -1,8 +1,17 @@
+import requests
+
 """ There is at least one baby name from the top 40 baby names for 2020 that, when spelled backwards, is a valid Scrabble word. Find and print all such names. """
+
+URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+r = requests.get(url = URL)
 
 with open('bNames2020.txt', 'r') as f:
     names = f.read().splitlines()
     
     for i in range(len(names)):
         rev = names[i][::-1].lower()
-        print(rev)
+        r = requests.get(url = URL + rev)
+        if r.status_code == 200:
+            print(names[i], rev)
+        
+        
