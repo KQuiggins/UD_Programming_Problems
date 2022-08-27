fd0874564fb329368c357923befb9f11213f3146
KQuiggins <kashfamilyvalues@gmail.com>
2022-08-27 07:39:28 -0400

    completed problem 4 in BabyNames

--- practice_problems/BabyNames/prob_4_bNames.py
+++ practice_problems/BabyNames/prob_4_bNames.py
@@ -1,2 +1,12 @@
 """ What are all of the names that were top 40 baby names in both 1880 and 2020? """
 
+with open('bNames1880.txt', 'r') as f1, \
+open('bNames2020.txt', 'r') as f2:
+    names1 = f1.read()
+    names2 = f2.read()
+    print(names1)
+    print(names2)
+
+    
+
+
